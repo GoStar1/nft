3953089176da44e415bb3d4fecb952d7f54f9fc5
hh-neo <hh13011315629@163.com>
2025-09-29 10:22:19 +0800

    1
    1

--- context/constants.js
+++ context/constants.js
@@ -1,4 +1,4 @@
 import marketAbi from './NFTMarketplace.json';
 
-export const MarketAddress = '0xF5f6B924332C350E3Fcd3A50Fc94db822f0B760f';
+export const MarketAddress = '0xf1ad1f9b24cedb9f55c19171b0f7b1c259deeb6d';
 export const MarketAddressABI = marketAbi.abi;
